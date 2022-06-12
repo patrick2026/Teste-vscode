ced09ac252460a8990734a54f75d08486bf46834
patrick2026 <106341384+patrick2026@users.noreply.github.com>
2022-06-12 09:59:18 -0300

    Update index.js

--- index.js
+++ index.js
@@ -1,7 +1,6 @@
 function imprimirDados(dados) {
-    console.log(dados)
+    console.log(dados())
 }
-imprimirDados(1)
-imprimirDados('testo')
-imprimirDados(true)
-imprimirDados9({1, 2, 3})
+imprimirDados(function () {
+retun 'ola mundo'
+} )
